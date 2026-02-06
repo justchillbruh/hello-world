ac81e9fdb83dd4f32c54511bce17c3075de90e25
justchillbruh <72055071+justchillbruh@users.noreply.github.com>
2026-02-06 11:14:10 -0500

    Added hello world rust file

---
+++ hello.rs
@@ -0,0 +1,10 @@
+// Author: Jesse - jsg59
+// Class: YWCC-307-006
+
+// This is an example of a comment
+
+fn main() {
+    println!("Hello World!");
+    println!("Author: Jesse G - jsg59");
+    println!("Class: YWCC-307-006");
+}
